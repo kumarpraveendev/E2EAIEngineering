5d25f37d809596dc0953dca612342bc1fd0da632
kumarpraveendev <praveen.git@gmail.com>
2026-07-29 03:02:42 +0200

    week3 complete

--- apps/api/src/api/agents/agents.py
+++ apps/api/src/api/agents/agents.py
@@ -1,34 +1,125 @@
+from langsmith import traceable,get_current_run_tree
+from langchain_core.messages import SystemMessage,convert_to_openai_messages
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel,Field
+import instructor
+from api.agents.utils.prompt_management import prompt_template_config
+from api.agents.tools import get_formatted_item_context
 
-from google import genai
-from groq import Groq
-from openai import OpenAI
-from api.core.config import config
+### QnA Agent Response Model
+
+class RAGUsedContext(BaseModel):
+    id: str = Field(description="ID of the item used to answer the question")
+    description: str = Field(description="Description of the item used to answer the question")
+
+class FinalResponse(BaseModel):
+    answer: str = Field(description="Answer to the question")
+    references: list[RAGUsedContext] = Field(description="List of items used to answer the question")
+
+### Intent Router Response Model
+
+class IntentRouterResponse(BaseModel):
+    question_relevant: bool
+    answer: str = Field(description="An answer to the question if the users question is not relevant to the products.")
 
 
-def run_llm(provider: str, model_name: str, messages: list[dict], max_tokens: int = 500):
-    if provider == "openai":
-        client = OpenAI(api_key=config.OPENAI_API_KEY)
-    elif provider == "groq":
-        client = Groq(api_key=config.GROQ_API_KEY)
-    else:
-        client = genai.Client(api_key=config.GOOGLE_API_KEY)
+### QnA Agent Node starts
 
-    if provider == "google":
-        return client.models.generate_content(
-            model=model_name,
-            contents=[message["content"] for message in messages],
-        ).text
 
-    if provider == "openai":
-        return client.chat.completions.create(
-            model=model_name,
-            messages=messages,
-            reasoning_effort="minimal",
-            max_completion_tokens=max_tokens,
-        ).choices[0].message.content
+def agent_node(state) -> dict:
 
-    return client.chat.completions.create(
-        model=model_name,
-        messages=messages,
-        max_tokens=max_tokens,
-    ).choices[0].message.content
+    template=prompt_template_config("api/agents/prompts/qna_agent.yaml", "qna_agent")
+
+    prompt = template.render()
+
+    llm = ChatOpenAI(
+        model="gpt-5.4-mini",
+        reasoning_effort="none",
+        use_responses_api=True
+    )
+    llm_with_tools = llm.bind_tools(
+        [get_formatted_item_context, FinalResponse],
+        tool_choice="any"
+    )
+
+    response = llm_with_tools.invoke(
+        [
+            SystemMessage(content=prompt),
+            *state.messages
+        ]
+    )
+
+    current_run = get_current_run_tree()
+    if current_run:
+        current_run.metadata["usage_metadata"] = {
+            "input_tokens": response.usage_metadata["input_tokens"],
+            "output_tokens": response.usage_metadata["output_tokens"],
+            "total_tokens": response.usage_metadata["total_tokens"],
+        }
+
+    final_answer = False
+    answer = ""
+    references = []
+
+    if len(response.tool_calls) > 0:
+        for tool_call in response.tool_calls:
+            if tool_call.get("name") == "FinalResponse":
+                final_answer = True
+                answer = tool_call.get("args").get("answer")
+                references.extend(tool_call.get("args").get("references"))
+
+    return {
+        "messages": [response],
+        "final_answer": final_answer,
+        "iteration": state.iteration + 1,
+        "answer": answer,
+        "references": references
+    }
+
+
+### Intent Router Node starts
+
+
+@traceable(
+    name="route_intent",
+    run_type="llm",
+    metadata={
+        "ls_provider": "openai",
+        "ls_model_name": "gpt-5.4-mini"
+    }
+)
+
+def intent_router_node(state) -> dict:
+
+    template=prompt_template_config("api/agents/prompts/intent_router_agent.yaml", "intent_router_agent")
+
+    prompt = template.render()
+
+    messages = state.messages
+
+    conversation = []
+
+    for message in messages:
+        conversation.append(convert_to_openai_messages(message))
+
+    client = instructor.from_provider(
+        "openai/gpt-5.4-mini",
+        mode=instructor.Mode.RESPONSES_TOOLS
+    )
+
+    response, raw_response = client.create_with_completion(
+        messages=[
+            {"role": "system", "content": prompt},
+            *conversation
+        ],
+        reasoning={"effort": "none"},
+        response_model=IntentRouterResponse
+    )
+    
+
+    return {
+        "question_relevant": response.question_relevant,
+        "answer": response.answer
+    }
+
+
